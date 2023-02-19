991d7ae1fdfe3c4d140f814bff4b07e3c78f07bf
technexx <48839830+technexx@users.noreply.github.com>
2023-02-19 12:22:27 -0800

    Initial commit

---
+++ app/src/main/java/a/third/jetpack/compose/myapplication/MainActivity.kt
@@ -0,0 +1,79 @@
+package a.third.jetpack.compose.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import a.third.jetpack.compose.myapplication.ui.theme.AThirdJetpackComposeStoryTheme
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintLayout
+
+//Todo: Card based? Follow story w/ different rounds. "Who are you" theme (Planescape: Torment)ish? With characters + helpers.
+    //Todo: Ancestral traits/cards to start, since current slate is blank.
+//Todo: Dating profile generator
+    //Todo: Input (selection) of traits/likes/dislikes/etc.
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AThirdJetpackComposeStoryTheme {
+
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background) {
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun FullView() {
+    ConstraintLayout {
+        val (statsAndBody, playArea) = createRefs()
+
+        val startGuideline = createGuidelineFromTop(0.3f)
+
+        StatsAndBody(modifier = Modifier
+            .constrainAs(statsAndBody) {
+                top.linkTo(startGuideline)
+            }
+        )
+
+    }
+}
+
+@Composable
+fun StatsAndBody(modifier: Modifier) {
+    ConstraintLayout {
+        //Constrains our surface as done in FullView()
+        Surface(modifier = modifier
+
+        ){
+
+        }
+    }
+}
+
+@Composable
+fun PlayArea() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AThirdJetpackComposeStoryTheme {
+
+    }
+}
