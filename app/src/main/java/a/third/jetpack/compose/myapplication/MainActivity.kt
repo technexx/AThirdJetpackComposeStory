811358176e57423f1c5cf11e7bd025fe51878b67
technexx <48839830+technexx@users.noreply.github.com>
2023-02-19 12:41:14 -0800

    image added

--- app/src/main/java/a/third/jetpack/compose/myapplication/MainActivity.kt
+++ app/src/main/java/a/third/jetpack/compose/myapplication/MainActivity.kt
@@ -11,7 +11,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import a.third.jetpack.compose.myapplication.ui.theme.AThirdJetpackComposeStoryTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -30,7 +36,8 @@ class MainActivity : ComponentActivity() {
 
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background) {
+                    color = Color.White) {
+                    FullView()
                 }
             }
         }
@@ -57,10 +64,14 @@ fun FullView() {
 fun StatsAndBody(modifier: Modifier) {
     ConstraintLayout {
         //Constrains our surface as done in FullView()
-        Surface(modifier = modifier
-
-        ){
-
+        Surface(modifier = modifier) {
+            //Todo: Image visible, but very dark background.
+            Image(
+                painter = painterResource(id = R.drawable.boxman),
+                contentDescription = "Boxman",
+                modifier = Modifier
+                    .size(80.dp)
+            )
         }
     }
 }
