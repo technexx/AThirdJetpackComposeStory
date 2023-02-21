c39e2d84c2898824a8395ab61600f796a4d52fa0
technexx <48839830+technexx@users.noreply.github.com>
2023-02-20 17:56:46 -0800

    fixed button sizing

--- app/src/main/java/a/third/jetpack/compose/myapplication/MainActivity.kt
+++ app/src/main/java/a/third/jetpack/compose/myapplication/MainActivity.kt
@@ -3,15 +3,13 @@ package a.third.jetpack.compose.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import a.third.jetpack.compose.myapplication.ui.theme.AThirdJetpackComposeStoryTheme
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -52,6 +50,7 @@ fun FullView() {
         val startGuideline = createGuidelineFromTop(0.2f)
         val (statsLayout, boardLayout) = createRefs()
 
+        //Todo: Should Column come before surface?
         //Stats surface.
         Surface(color = MaterialTheme.colors.primary,
             modifier = Modifier
@@ -78,16 +77,28 @@ fun FullView() {
             }
         }
 
-        Surface(color = MaterialTheme.colors.secondary,
-            modifier = Modifier
-                .constrainAs(boardLayout) {
-                    top.linkTo(statsLayout.bottom)
-                }
-                .fillMaxSize()
-        ) {
-
+        Surface(modifier = Modifier
+            .constrainAs(boardLayout) {
+                top.linkTo(statsLayout.bottom)
+            }
+            .fillMaxSize(),
+            color = MaterialTheme.colors.onBackground) {
+            //Todo: Button fills entire screen
+            Button(
+                //requiredSize works where size does not
+                modifier = Modifier
+                .requiredSize(120.dp, 40.dp),
+                onClick = {
+                    testClick()
+                }) {
+                Text(text = "Click me!")
+            }
         }
     }
+}
+
+private fun testClick() {
+
 }
 
 @Preview(showBackground = true)
